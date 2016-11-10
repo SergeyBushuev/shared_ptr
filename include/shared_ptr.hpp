81b51a558abbfb52de0f8638201523dd7793a97f
Varvar <Sergeybushuev14@gmail.com>
2016-11-10 14:02:47 +0400

    Update shared_ptr.hpp

--- include/shared_ptr.hpp
+++ include/shared_ptr.hpp
@@ -1,1 +1,21 @@
+template<typename T>
+class shared_ptr {
+public:
+   explicit shared_ptr(T * ptr = nullptr);
 
+   shared_ptr( shared_ptr const & );
+   auto operator =( shared_ptr const & ) -> shared_ptr &;
+   shared_ptr( shared_ptr && );
+   auto operator =( shared_ptr && ) -> shared_ptr&;
+   ~shared_ptr();
+
+   auto reset() -> void;
+
+   auto get() -> T *;
+   auto use_count() -> size_t;
+   auto unique() -> bool;
+
+   auto operator*() -> T &;
+   auto operator->() -> T *;  
+   operator bool();
+};
